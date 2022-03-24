f826a3bf8f5651194ea5aa1bea972e6ffa4ca262
hogehoge <elementrd6@hotmail.com>
2022-03-24 14:05:33 +0900

    コンソールには表れず

---
+++ Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldManager : MonoBehaviour
+{
+    InputField inputField;
+
+    void Start()
+    {
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        
+    }
+public void GetInputName()
+    {
+        string name = inputField.text;
+        Debug.Log(name);
+
+        inputField.text = "";
+    }
+}
