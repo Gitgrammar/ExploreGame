e60243439844f1ca7b21cb093e77be82c9e51818
hogehoge <elementrd6@hotmail.com>
2022-03-24 13:44:48 +0900

    inputScreen追加

---
+++ Assets/Scripts/InputBirdsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InputBirdsManager : MonoBehaviour
+{
+    InputField inputField;
+
+    void Start()
+    {
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("ReturnToUserFillOut", 0.5f);
+            Destroy(gameObject, 0.5f);
+        }
+
+    }
+    void ReturnToUserFillOut()
+    {
+        SceneManager.LoadScene("Birds");
+
+    }
+
+
+    public void GetInputName() 
+    {
+        string name = inputField.text;
+        Debug.Log(name);
+        inputField.text = "";
+    }
+}
