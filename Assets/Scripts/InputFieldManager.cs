76e88e1ac6a565f4ba33200de03b104f5627b9d1
hogehoge <elementrd6@hotmail.com>
2022-03-24 14:38:05 +0900

    コンソールで正解までは出せる。

--- Assets/Scripts/InputFieldManager.cs
+++ Assets/Scripts/InputFieldManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class InputFieldManager : MonoBehaviour
 {
     InputField inputField;
+    string[] creature = { "birds", "mammals", "lizard", "fish" };
 
     void Start()
     {
@@ -16,7 +18,12 @@ public void GetInputName()
     {
         string name = inputField.text;
         Debug.Log(name);
+        if (creature.Contains(name))
+        {
+            Debug.Log("correct");
+        }
 
         inputField.text = "";
     }
+    
 }
