ca10d3c6be7290ec70b5a3d2eb5958d639c8d7d4
hogehoge <elementrd6@hotmail.com>
2022-03-27 12:06:40 +0900

    間違いシーン追加

--- Assets/Scripts/InputFieldManager.cs
+++ Assets/Scripts/InputFieldManager.cs
@@ -3,33 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class InputFieldManager : MonoBehaviour
 {
     InputField inputField;
     string[] creature = { "birds", "mammals", "lizard", "fish" };
-    public Text TextFrame;
-    private string name = "";
+
 
     void Start()
     {
         inputField = GameObject.Find("InputField").GetComponent<InputField>();
-        
+
     }
-public void GetInputName()
+    public void GetInputName()
     {
-        name = inputField.text;
+        string name = inputField.text;
         Debug.LogFormat(name);
         if (creature.Contains(name))
         {
+            
             Debug.LogFormat("Correct!");
+            Invoke("ToCorrectScene", 1.5f);
         }
         else
         {
             Debug.LogFormat("Wrong!");
+            Invoke("ToWrongScene", 1.5f);
         }
 
+
         inputField.text = "";
+
     }
-    
+    void ToWrongeScene() { 
+    SceneManager.LoadScene("CasesOfWrong");
+        }
+    void ToCorrectScene()
+    {
+        SceneManager.LoadScene("CasesOfCorrect");
+    }
+
 }
