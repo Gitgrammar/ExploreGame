a4a5cc88c3636a027f8f36231a0f092f1038ee72
hogehoge <elementrd6@hotmail.com>
2022-04-02 19:25:14 +0900

    .git 外す

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Attack : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("ReturnToUserFillOut", 0.5f);
+            Destroy(gameObject, 0.5f);
+        }
+
+    }
+    void ReturnToUserFillOut()
+    {
+        SceneManager.LoadScene("UserEntry");
+
+    }
+}
---
+++ Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class InputFieldManager : MonoBehaviour
+{
+    InputField inputField;
+    string[] creature = { "bird", "mammal", "lizard", "fish","vegetable","fruit","egg","crustacean" };
+
+
+    void Start()
+    {
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+
+    }
+    public void GetInputName()
+    {
+        string name = inputField.text;
+        Debug.LogFormat(name);
+        if (creature.Contains(name))
+        {
+            
+            Debug.LogFormat("Correct!");
+            Invoke("ToCorrectScene", 1.5f);
+        }
+        else
+        {
+            Debug.LogFormat("Wrong!");
+            Invoke("ToWrongScene", 1.5f);
+        }
+
+
+        inputField.text = "";
+
+    }
+    void ToWrongScene() { 
+    SceneManager.LoadScene("CasesOfWrong");
+        }
+    void ToCorrectScene()
+    {
+        SceneManager.LoadScene("CasesOfCorrect");
+    }
+
+}
