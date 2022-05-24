c8a51f07c2b36d83e199e40f898deb1165d8a73e
hogehoge <elementrd6@hotmail.com>
2022-05-24 13:38:08 +0900

    nameにstaticつけてシーンを跨いで値を保持

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -6,18 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class Attack : MonoBehaviour
 {
+    static string name;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Invoke("ReturnToUserFillOut", 0.5f);
             Destroy(gameObject, 0.5f);
+            name = "tiger";
         }
 
     }
     void ReturnToUserFillOut()
     {
         SceneManager.LoadScene("UserEntry");
+        Debug.Log(name);
 
     }
 }
