415b666acb70581059ff57df3b211a2c774cb397
hogehoge <elementrd6@hotmail.com>
2022-05-28 19:27:30 +0900

    CreatureTransfer追加

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -6,23 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class Attack : MonoBehaviour
 {
-    static Animator creature;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Invoke("ReturnToUserFillOut", 0.5f);
-            Destroy(gameObject, 0.5f);
-            creature = GetComponent<Animator>();
         }
 
     }
     void ReturnToUserFillOut()
     {
-        DontDestroyOnLoad(gameObject);
             SceneManager.LoadScene("UserEntry");
-        creature.transform.localPosition=new Vector3 (10, 200,100);
-        Debug.Log(creature);
-
+        Debug.Log(gameObject);
+        
     }
 }
